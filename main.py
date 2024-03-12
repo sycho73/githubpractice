444e8971827027e15639f4d1fc4322a8ce456d70
sycho73 <opeinfo73@gmail.com>
2024-03-12 14:12:21 +0900

    조성열 2020112060

---
+++ main.py
@@ -0,0 +1,10 @@
+def main():
+    name = input("이름을 입력하세요: ")
+    studentid = input("학번을 입력하세요: ")
+    studentclass = input("학과를 입력하세요: ")
+    print("이름:", name)
+    print("학번:", studentid)
+    print("학과:",studentclass)
+
+if __name__ == "__main__":
+    main()
